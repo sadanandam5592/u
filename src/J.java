2ae248943698662d5563d503495ba5bf8156b794
SusmithGU <susmitha.gundu@gmail.com>
2022-04-20 16:58:48 +0530

    3

--- src/J.java
+++ src/J.java
@@ -1,14 +1,17 @@
 public class J {
     stage.status = status
-          if(status.isComplete){
-        stage.context["exception"]=HashMap<String, Any?>().apply{
-            this["details"]= HashMap<String, Any?>().apply{
-                this["errors"] = ArrayList<String>().apply{
-                    stage.tasks.filter {t->t.exceptionDetails.isNotEmpty()  }.forEach{
-                        val details= (it.exceptionDetails["exception"] as HashMap<*,*>)["details"] as HashMap<*,*>
-                        this.add(it.name+":")
+          if (status.isComplete) {
+        stage.context["exception"] = HashMap<String, Any>().apply {
+            this["details"] = HashMap<String, Any>().apply {
+                this["errors"] = ArrayList<String>().apply {
+                    stage.tasks.filter { t -> t.exceptionDetails.isNotEmpty() }.forEach {
+                        val details = (it.exceptionDetails["exception"] as HashMap<*, *>)["details"] as HashMap<*, *>
+                        this.add(it.name + ":")
                         this.add((details["error"] as String))
-                        (details["errors"] as ArrayList<String>).forEach { t->this.add(t) }
-                    } }}}}
-
+                        (details["errors"] as ArrayList<String>).forEach { t -> this.add(t) }
+                    }
+                }
+            }
+        }
+    }
 }
